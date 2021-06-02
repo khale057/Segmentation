ef3f11d113c4f1852325631f263b1dc23c140abe
QingC0 <31938876+QingC0@users.noreply.github.com>
2021-06-02 10:32:49 -0400

    Update MakeTrain.py

--- data/MakeTrain.py
+++ data/MakeTrain.py
@@ -1,1 +1,17 @@
+import os
+import numpy as np 
+import random
+import shutil
 
+
+if len(os.listdir('test_data')) > 0:
+    os.system('mv ./test_data/* ./train_data/')
+    os.system('mv ./test_mask/* ./train_mask/')
+
+fs = ['vol47_recon_01400_RGB', 'vol47_recon_00584_RGB', 'vol55_recon_00936_RGB', 'vol41_recon_00535_RGB', 'vol32_recon_00204_RGB', 'vol55_recon_00611_RGB', 'vol55_recon_01704_RGB', 'vol32_recon_02105_RGB', 'vol47_recon_00047_RGB', 'vol32_recon_00535_RGB']
+
+# Choose one as the testing image, others as training dataset
+f = fs[2]
+
+os.system('mv ./train_data/'+f+'* ./test_data/')
+os.system('mv ./train_mask/'+f+'* ./test_mask/')
